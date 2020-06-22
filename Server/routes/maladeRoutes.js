31a114f704d6f82246f9f1600e4d86a99c1d418b
Raiah Mohamed Amine <hm_raiah@esi.dz>
2020-06-22 14:59:20 +0100

    Removing VerifyAuth MiddleWare

--- Server/routes/maladeRoutes.js
+++ Server/routes/maladeRoutes.js
@@ -2,7 +2,7 @@ const Malade = require('../handlers/maladeHandlers/index');
 const VerifyToken = require('../handlers/adminHandlers/Verifytoken');
 
 module.exports =(app)=>{
-    app.post('/malade/add',VerifyToken,Malade.AddMalade);
-    app.post('/malade/get',VerifyToken,Malade.GetMalades);
-    app.post('/malade/get/:etat',VerifyToken,Malade.GetMaladesEtat);
+    app.post('/malade/add',Malade.AddMalade);
+    app.post('/malade/get',Malade.GetMalades);
+    app.post('/malade/get/:etat',Malade.GetMaladesEtat);
 }
