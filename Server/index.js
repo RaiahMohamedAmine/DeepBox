f7cb113d027c620bb2097dcc8c9cc25af6ac3be0
BillelBenoudjit <hb_benoudjit@esi.dz>
2020-06-22 15:39:49 +0100

    Add cors to allow access to server side

--- Server/index.js
+++ Server/index.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+const cors = require("cors");
+
 
 app.use(bodyParser.json());
+app.use(cors());
+
 require('./db');
 require('./routes/maladeRoutes')(app);
 require('./routes/adminRoutes')(app);
