e6d06d0b78b5e4f902b262b9d52559ff2d9a775d
BillelBenoudjit <hb_benoudjit@esi.dz>
2020-06-24 11:07:52 +0100

    Add pie chart to dashboard

--- ClientSide/src/Demo/Charts/Nvd3Chart/PieBasicChart.js
+++ ClientSide/src/Demo/Charts/Nvd3Chart/PieBasicChart.js
@@ -1,21 +1,73 @@
 import React from 'react';
+import axios from 'axios'
+import { Spinner } from 'react-bootstrap'
 import NVD3Chart from 'react-nvd3';
 
-const datum = [
-    {key: "One", y: 29, color: "#ff8a65"},
-    {key: "Two", y: 0, color: "#f4c22b"},
-    {key: "Three", y: 32, color: "#04a9f5"},
-    {key: "Four", y: 196, color: "#3ebfea"},
-    {key: "Five", y: 2, color: "#4F5467"},
-    {key: "Six", y: 98, color: "#1de9b6"},
-    {key: "Seven", y: 13, color: "#a389d4"},
-    {key: "Eight", y: 5, color: "#FE8A7D"}
+var datum = [
+    { key: "Suspects", y: 0, color: "#ff8a65" },
+    { key: "Positifs", y: 0, color: "#f4c22b" },
+    { key: "Négatifs", y: 0, color: "#04a9f5" },
+    { key: "Guéris", y: 0, color: "#3ebfea" }
 ];
 
 class PieBasicChart extends React.Component {
+    state = {
+        suspects: [],
+        positifCases: [],
+        negatifCases: [],
+        healdCases: [],
+        loading: 0,
+    }
+
+    componentWillMount() {
+        this.refreshMalades()
+    }
+
+    refreshMalades() {
+        axios.get('http://localhost:5200/malade/get/Suspect').then((response) => {
+            this.setState({
+                suspects: response.data.malades,
+                loading: this.state.loading + 1
+            }, () => {
+                datum[0].y = this.state.suspects.length
+            })
+        })
+        axios.get('http://localhost:5200/malade/get/Positif').then((response) => {
+            this.setState({
+                positifCases: response.data.malades,
+                loading: this.state.loading + 1
+            }, () => {
+                datum[1].y = this.state.positifCases.length
+            })
+        })
+        axios.get('http://localhost:5200/malade/get/Negatif').then((response) => {
+            this.setState({
+                negatifCases: response.data.malades,
+                loading: this.state.loading + 1
+            }, () => {
+                datum[2].y = this.state.negatifCases.length
+            })
+        })
+        axios.get('http://localhost:5200/malade/get/Gueri').then((response) => {
+            this.setState({
+                healdCases: response.data.malades,
+                loading: this.state.loading + 1
+            }, () => {
+                datum[3].y = this.state.healdCases.length
+            })
+        })
+    }
 
     render() {
-        return <NVD3Chart id="chart" height={300} type="pieChart" datum={datum} x="key" y="y"  />
+        return (
+            this.state.loading >= 4 ?
+                <NVD3Chart id="chart" height={300} type="pieChart" datum={datum} x="key" y="y" />
+             :
+        <Spinner animation="border" role="status">
+            <span className="sr-only">Loading...</span>
+        </Spinner>
+        
+        )
     }
 }
 
