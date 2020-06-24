3910289553840354b46925af59cba53aee5df764
BillelBenoudjit <hb_benoudjit@esi.dz>
2020-06-24 14:46:48 +0100

    Add this week patients

--- ClientSide/src/Demo/Dashboard/Default.js
+++ ClientSide/src/Demo/Dashboard/Default.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Row, Col, Card, Table, Spinner, Tabs, Tab } from 'react-bootstrap';
 import axios from 'axios'
+import moment from 'moment'
 
 import Aux from "../../hoc/_Aux";
 import DEMO from "../../store/constant";
@@ -62,34 +63,72 @@ class Dashboard extends React.Component {
         let CasesSameDay = this.state.malades.map((malade, index) => {
             let date = malade.dateAjout
             let today = new Date()
-            console.log("User : " + index + " Date user month : " + date.slice(8))
-            console.log(today.getDay())
-            if (parseInt(date.slice(0,4)) == today.getFullYear()) {
-                if (parseInt(date.slice(5,7)) == today.getMonth() + 1) {
+            if (parseInt(date.slice(0, 4)) == today.getFullYear()) {
+                if (parseInt(date.slice(5, 7)) == today.getMonth() + 1) {
                     if (parseInt(date.slice(8)) == parseInt(today.getDate())) {
                         if (malade.etat == "Positif" || malade.etat == "Négatif") {
                             return (
                                 <div key={index} className="media friendlist-box align-items-center justify-content-center m-b-20">
-                                <div className="m-r-10 photo-table">
-                                    {malade.sexe == "Homme" ? 
-                                    <a href={DEMO.BLANK_LINK}><img className="rounded-circle" style={{ width: '40px' }} src={avatar2} alt="activity-user" /></a>
-                                    :
-                                    <a href={DEMO.BLANK_LINK}><img className="rounded-circle" style={{ width: '40px' }} src={avatar1} alt="activity-user" /></a>
-                                    }
-                                    
+                                    <div className="m-r-10 photo-table">
+                                        {malade.sexe == "Homme" ?
+                                            <a href={DEMO.BLANK_LINK}><img className="rounded-circle" style={{ width: '40px' }} src={avatar2} alt="activity-user" /></a>
+                                            :
+                                            <a href={DEMO.BLANK_LINK}><img className="rounded-circle" style={{ width: '40px' }} src={avatar1} alt="activity-user" /></a>
+                                        }
+
+                                    </div>
+                                    <div className="media-body">
+                                        <h6 className="m-0 d-inline">{malade.nom + " " + malade.prenom}</h6>
+                                        {malade.etat == "Positif" ?
+                                            <span className="float-right d-flex  align-items-center"><i className="fa fa-plus f-22 m-r-10 text-c-red" /><b>Positif</b></span>
+                                            :
+                                            <span className="float-right d-flex  align-items-center"><i className="fa fa-minus f-22 m-r-10 text-c-green" /><b>Négatif</b></span>
+                                        }
+                                    </div>
                                 </div>
-                                <div className="media-body">
-                                    <h6 className="m-0 d-inline">{malade.nom + " " + malade.prenom}</h6>
-                                    {malade.etat == "Positif" ?
-                                    <span className="float-right d-flex  align-items-center"><i className="fa fa-plus f-22 m-r-10 text-c-red" /><b>Positif</b></span>
-                                    :
-                                    <span className="float-right d-flex  align-items-center"><i className="fa fa-minus f-22 m-r-10 text-c-green" /><b>Négatif</b></span>
-                                    }
-                                </div>
-                            </div>
                             )
                         }
                     }
+                }
+            }
+        })
+
+        let CasesSameWeek = this.state.malades.map((malade, index) => {
+            let date = malade.dateAjout
+            var today = new Date();
+            var dd = today.getDate();
+            var mm = today.getMonth() + 1;
+            var yyyy = today.getFullYear();
+            if (dd < 10) {
+                dd = '0' + dd;
+            }
+            if (mm < 10) {
+                mm = '0' + mm;
+            }
+            today = yyyy + '-' + mm + '-' + dd;
+            console.log("Date : " + date + " Today : " + today)
+            if (moment(date).isSame((today), 'week')) {
+                if (malade.etat == "Positif" || malade.etat == "Négatif") {
+                    return (
+                        <div key={index} className="media friendlist-box align-items-center justify-content-center m-b-20">
+                            <div className="m-r-10 photo-table">
+                                {malade.sexe == "Homme" ?
+                                    <a href={DEMO.BLANK_LINK}><img className="rounded-circle" style={{ width: '40px' }} src={avatar2} alt="activity-user" /></a>
+                                    :
+                                    <a href={DEMO.BLANK_LINK}><img className="rounded-circle" style={{ width: '40px' }} src={avatar1} alt="activity-user" /></a>
+                                }
+
+                            </div>
+                            <div className="media-body">
+                                <h6 className="m-0 d-inline">{malade.nom + " " + malade.prenom}</h6>
+                                {malade.etat == "Positif" ?
+                                    <span className="float-right d-flex  align-items-center"><i className="fa fa-plus f-22 m-r-10 text-c-red" /><b>Positif</b></span>
+                                    :
+                                    <span className="float-right d-flex  align-items-center"><i className="fa fa-minus f-22 m-r-10 text-c-green" /><b>Négatif</b></span>
+                                }
+                            </div>
+                        </div>
+                    )
                 }
             }
         })
@@ -155,102 +194,99 @@ class Dashboard extends React.Component {
 
         return (
             <>
-            <Aux>
-                <Row>
-                    <Col md={6}>
-                        <Card>
-                            <Card.Header>
-                                <Card.Title as="h5">Répartition des cas par gravité</Card.Title>
-                            </Card.Header>
-                            <Card.Body>
-                                <PieBasicChart />
-                            </Card.Body>
-                        </Card>
-                    </Col>
-                    <Col md={6}>
-                        <Card>
-                            <Card.Header>
-                                <Card.Title as="h5">Répartition des cas par sexe</Card.Title>
-                            </Card.Header>
-                            <Card.Body>
-                                <BarDiscreteChart />
-                            </Card.Body>
-                        </Card>
-                    </Col>
-                </Row>
-                <Row>
-                    <Col md={6} xl={8} className='m-b-30'>
-                        <Tabs defaultActiveKey="today" id="uncontrolled-tab-example">
-                            <Tab eventKey="today" title="Today">
-                                {CasesSameDay}
-                            </Tab>
-                            <Tab eventKey="week" title="This Week">
-                                {tabContent}
-                            </Tab>
-                            <Tab eventKey="all" title="All">
-                                {tabContent}
-                            </Tab>
-                        </Tabs>
-                    </Col>
-                    <Col md={6} xl={4}>
-                        <Card>
-                            {
-                                this.state.loading >= 4 ?
-                                <>
-                                    <Card.Body className='border-bottom'>
-                                        <div className="row d-flex align-items-center">
-                                            <div className="col-auto">
-                                                <i className="feather icon-zap f-30 text-c-yellow" />
-                                            </div>
-                                            <div className="col">
-                                                <h3 className="f-w-300">{this.state.healdCases.length}</h3>
-                                                <span className="d-block text-uppercase">Nombre de géris</span>
-                                            </div>
-                                        </div>
-                                    </Card.Body>
-                                    <Card.Body>
-                                        <div className="row d-flex align-items-center">
-                                            <div className="col-auto">
-                                                <i className="feather icon-alert-circle f-30 text-c-black" />
-                                            </div>
-                                            <div className="col">
-                                                <h3 className="f-w-300">{this.state.suspects.length}</h3>
-                                                <span className="d-block text-uppercase">Nombre de suspects</span>
-                                            </div>
-                                        </div>
-                                    </Card.Body>
-                                    <Card.Body className='border-bottom'>
-                                        <div className="row d-flex align-items-center">
-                                            <div className="col-auto">
-                                                <i className="feather icon-plus f-30 text-c-red" />
-                                            </div>
-                                            <div className="col">
-                                                <h3 className="f-w-300">{this.state.positifCases.length}</h3>
-                                                <span className="d-block text-uppercase">Nombre de testés positif</span>
-                                            </div>
-                                        </div>
-                                    </Card.Body>
-                                    <Card.Body className='border-bottom'>
-                                        <div className="row d-flex align-items-center">
-                                            <div className="col-auto">
-                                                <i className="feather icon-minus f-30 text-c-green" />
-                                            </div>
-                                            <div className="col">
-                                                <h3 className="f-w-300">{this.state.negatifCases.length}</h3>
-                                                <span className="d-block text-uppercase">Nombre de testés négatif</span>
-                                            </div>
-                                        </div>
-                                    </Card.Body>
-                                </>
-                                : <Spinner animation="border" role="status">
-                                        <span className="sr-only">Loading...</span>
-                                    </Spinner>
-                            }
-                        </Card>
-                    </Col>
+                <Aux>
+                    <Row>
+                        <Col md={6}>
+                            <Card>
+                                <Card.Header>
+                                    <Card.Title as="h5">Répartition des cas par gravité</Card.Title>
+                                </Card.Header>
+                                <Card.Body>
+                                    <PieBasicChart />
+                                </Card.Body>
+                            </Card>
+                        </Col>
+                        <Col md={6}>
+                            <Card>
+                                <Card.Header>
+                                    <Card.Title as="h5">Répartition des cas par sexe</Card.Title>
+                                </Card.Header>
+                                <Card.Body>
+                                    <BarDiscreteChart />
+                                </Card.Body>
+                            </Card>
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col md={6} xl={8} className='m-b-30'>
+                            <Tabs defaultActiveKey="today" id="uncontrolled-tab-example">
+                                <Tab eventKey="today" title="Today">
+                                    {CasesSameDay}
+                                </Tab>
+                                <Tab eventKey="week" title="This Week">
+                                    {CasesSameWeek}
+                                </Tab>
+                            </Tabs>
+                        </Col>
+                        <Col md={6} xl={4}>
+                            <Card>
+                                {
+                                    this.state.loading >= 4 ?
+                                        <>
+                                            <Card.Body className='border-bottom'>
+                                                <div className="row d-flex align-items-center">
+                                                    <div className="col-auto">
+                                                        <i className="feather icon-zap f-30 text-c-yellow" />
+                                                    </div>
+                                                    <div className="col">
+                                                        <h3 className="f-w-300">{this.state.healdCases.length}</h3>
+                                                        <span className="d-block text-uppercase">Nombre de géris</span>
+                                                    </div>
+                                                </div>
+                                            </Card.Body>
+                                            <Card.Body>
+                                                <div className="row d-flex align-items-center">
+                                                    <div className="col-auto">
+                                                        <i className="feather icon-alert-circle f-30 text-c-black" />
+                                                    </div>
+                                                    <div className="col">
+                                                        <h3 className="f-w-300">{this.state.suspects.length}</h3>
+                                                        <span className="d-block text-uppercase">Nombre de suspects</span>
+                                                    </div>
+                                                </div>
+                                            </Card.Body>
+                                            <Card.Body className='border-bottom'>
+                                                <div className="row d-flex align-items-center">
+                                                    <div className="col-auto">
+                                                        <i className="feather icon-plus f-30 text-c-red" />
+                                                    </div>
+                                                    <div className="col">
+                                                        <h3 className="f-w-300">{this.state.positifCases.length}</h3>
+                                                        <span className="d-block text-uppercase">Nombre de testés positif</span>
+                                                    </div>
+                                                </div>
+                                            </Card.Body>
+                                            <Card.Body className='border-bottom'>
+                                                <div className="row d-flex align-items-center">
+                                                    <div className="col-auto">
+                                                        <i className="feather icon-minus f-30 text-c-green" />
+                                                    </div>
+                                                    <div className="col">
+                                                        <h3 className="f-w-300">{this.state.negatifCases.length}</h3>
+                                                        <span className="d-block text-uppercase">Nombre de testés négatif</span>
+                                                    </div>
+                                                </div>
+                                            </Card.Body>
+                                        </>
+                                        : <Spinner animation="border" role="status">
+                                            <span className="sr-only">Loading...</span>
+                                        </Spinner>
+                                }
+                            </Card>
+                        </Col>
 
-                </Row>
-            </Aux>
+                    </Row>
+                </Aux>
             </>
         );
     }
