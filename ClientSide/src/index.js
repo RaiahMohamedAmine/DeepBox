bdd37ed82c335ccb1610261de3204cf914348f1e
Raiah Mohamed Amine <hm_raiah@esi.dz>
2020-06-26 22:31:51 +0100

    FIXING Store problem with Redux Toastr

--- ClientSide/src/index.js
+++ ClientSide/src/index.js
@@ -1,19 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore } from 'redux';
+import { createStore, combineReducers } from 'redux';
 import {Provider} from 'react-redux';
 import {BrowserRouter} from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
-
+import 'react-redux-toastr/lib/css/react-redux-toastr.min.css'
 import App from './App/index';
 import * as serviceWorker from './serviceWorker';
 import reducer from './store/reducer';
+import ReduxToastr from 'react-redux-toastr'
 import config from './config';
+import {reducer as toastsReducer} from 'react-redux-toastr';
 
-const store = createStore(reducer);
+const reducers = {
+    reducer,
+    toastr : toastsReducer
+};
+const TR = combineReducers(reducers);
+const store = createStore(TR);
 
 const app = (
     <Provider store={store}>
+        <ReduxToastr
+      timeOut={4000}
+      newestOnTop={false}
+      preventDuplicates
+      position="bottom-right"
+      transitionIn="fadeIn"
+      transitionOut="fadeOut"
+      progressBar
+      closeOnToastrClick/>
         <BrowserRouter basename={config.basename}>
             {/* basename="/datta-able" */}
             <App />
