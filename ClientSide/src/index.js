5128e81d11d86d6d18c37448d49c28c1b3502fa5
Raiah Mohamed Amine <hm_raiah@esi.dz>
2020-06-26 23:23:20 +0100

    Removing the toastr Reducer form store

--- ClientSide/src/index.js
+++ ClientSide/src/index.js
@@ -8,28 +8,18 @@ import 'react-redux-toastr/lib/css/react-redux-toastr.min.css'
 import App from './App/index';
 import * as serviceWorker from './serviceWorker';
 import reducer from './store/reducer';
-import ReduxToastr from 'react-redux-toastr'
+import ReduxToastr, {reducer as toastsReducer} from 'react-redux-toastr'
 import config from './config';
-import {reducer as toastsReducer} from 'react-redux-toastr';
 
 const reducers = {
     reducer,
     toastr : toastsReducer
 };
 const TR = combineReducers(reducers);
-const store = createStore(TR);
+const store = createStore(reducer);
 
 const app = (
     <Provider store={store}>
-        <ReduxToastr
-      timeOut={4000}
-      newestOnTop={false}
-      preventDuplicates
-      position="bottom-right"
-      transitionIn="fadeIn"
-      transitionOut="fadeOut"
-      progressBar
-      closeOnToastrClick/>
         <BrowserRouter basename={config.basename}>
             {/* basename="/datta-able" */}
             <App />
@@ -39,6 +29,19 @@ const app = (
 
 ReactDOM.render(app, document.getElementById('root'));
 
+/**
+ * 
+       <ReduxToastr
+      timeOut={4000}
+      newestOnTop={false}
+      preventDuplicates
+      position="bottom-right"
+      transitionIn="fadeIn"
+      transitionOut="fadeOut"
+      progressBar
+      closeOnToastrClick/>
+
+ */
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
