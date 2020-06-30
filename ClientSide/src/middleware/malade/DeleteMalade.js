224db7f427f6389831b70da33d0bed949739c8c6
Raiah Mohamed Amine <hm_raiah@esi.dz>
2020-06-30 12:19:08 +0100

    Delete Middleware added to front

---
+++ ClientSide/src/middleware/malade/DeleteMalade.js
@@ -0,0 +1,25 @@
+import axios from 'axios';
+import config from '../config';
+import {toastr} from 'react-redux-toastr'
+
+export default (data, token) => {
+    return axios ({
+        method: 'POST',
+        url : config.URL+ ":" + config.PORT +'/malade/delete',
+        data,
+        headers : {
+            authorization : 'Bearer '+ token
+        }
+    }).then (res=> {
+        if (res.data.type ==='Err'){
+            toastr.error('Erreur', "Une Erreur a survenu. Veuillez Réessayez")
+        }
+        else{
+           // toastr.success('Malade Ajouté', "Le malade "+data.prenom + ' ' + data.nom + ' a été ajouté avec Succés');
+            alert("Le malade "+data.prenom + ' ' + data.nom + " a été supprimé avec Succés")
+        }
+    }).catch (err=>{
+       // toastr.error ('Erreur Fatale !', 'Assurez-vous que le serveur est bien en marche');
+        alert('Assurez-vous que le serveur est bien en marche');
+    });
+}
