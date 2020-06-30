92ab998b3270ac4fa00b73e9cf4fe7085a9ed640
Raiah Mohamed Amine <hm_raiah@esi.dz>
2020-06-30 12:15:39 +0100

    Adding Delete in Backend

---
+++ Server/handlers/maladeHandlers/delete.js
@@ -0,0 +1,35 @@
+const connection = require('../../db');
+
+async function addMalade (req,res){
+    if (!req.body)
+    {
+        res.status(400).json({
+            type :"Err",
+            message : "Bad Request"
+        });
+        return;
+    }
+    connection.query ('DELETE FROM malade Where nom = '+ req.body.nom 
+    +' prenom = '+ req.body.prenom 
+    + 'adresse= ' + req.body.adresse
+    + 'siege= ' + req.body.siege
+    + 'dateAjout= ' + req.body.dateAjout
+    + 'sexe= ' + req.body.sexe
+    + 'etat= ' + req.body.etat
+    + 'tel= ' + req.body.tel, (err) => {
+        if (err) {
+            console.log(err)
+            res.status(200).json({
+                type:"Err",
+                message :"Server not responding"
+            });
+        } else{
+            res.status(200).json({
+                type :"Info",
+                message :"Malade Deleted successfully !"
+            });
+        }
+    });
+}
+
+module.exports = addMalade;
