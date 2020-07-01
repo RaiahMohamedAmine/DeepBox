3dbf18e0118be49203d9fdfcc7416d1c142acd63
Raiah Mohamed Amine <hm_raiah@esi.dz>
2020-07-01 12:14:11 +0100

    Implementing Modify BAckend middleware

---
+++ Server/handlers/maladeHandlers/modifyMalade.js
@@ -0,0 +1,28 @@
+const connection = require('../../db');
+
+async function DeleteMalade (req,res){
+    if (!req.body)
+    {
+        res.status(400).json({
+            type :"Err",
+            message : "Bad Request"
+        });
+        return;
+    }
+    connection.query ('UPDATE malade SET ?',req.body, (err) => {
+        if (err) {
+            res.status(200).json({
+                type:"Err",
+                message :"Server not responding"
+            });
+        } else{
+            console.log('deleted')
+            res.status(200).json({
+                type :"Info",
+                message :"Malade Deleted successfully !"
+            });
+        }
+    });
+}
+
+module.exports = DeleteMalade;
