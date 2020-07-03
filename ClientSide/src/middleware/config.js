63bd769ada21e717a959867a45c9a4d0f0b97090
Raiah Mohamed Amine <hm_raiah@esi.dz>
2020-07-03 11:56:30 +0100

    Adding Toast Options to config.js

--- ClientSide/src/middleware/config.js
+++ ClientSide/src/middleware/config.js
@@ -1,4 +1,13 @@
 export default {
     URL : 'http://localhost',
-    PORT : 5200 
+    PORT : 5200 ,
+    TOAST_OPTIONS :{
+        position: "top-center",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        }
 }
