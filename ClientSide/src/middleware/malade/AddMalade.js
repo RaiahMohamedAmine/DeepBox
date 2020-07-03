86d46e046e59302ecd591350895fa375e752fec4
Raiah Mohamed Amine <hm_raiah@esi.dz>
2020-07-03 12:00:16 +0100

    Adding Toast to AddMalade

--- ClientSide/src/middleware/malade/AddMalade.js
+++ ClientSide/src/middleware/malade/AddMalade.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
 import config from '../config';
+import {toast} from 'react-toastify'
 
 export default (data, token) => {
     return axios ({
@@ -11,17 +12,17 @@ export default (data, token) => {
         }
     }).then (res=> {
         if (res.data.type ==='Err'){
-            alert('Une Erreur a survenu. Veuillez Réessayez"');
+            toast.error('Une Erreur a survenu. Veuillez Réessayez !',config.TOAST_OPTIONS);
     }
     else{
         console.log(res.data)
         if (res.data.type==='ID-Err')
-            alert(res.data.message);
+            toast.error(res.data.message,config.TOAST_OPTIONS);
         else
-            alert("Le malade "+data.prenom + ' ' + data.nom + " a été ajouté avec Succés")
+            toast.success("Le malade "+data.prenom + ' ' + data.nom + " a été ajouté avec Succés",config.TOAST_OPTIONS)
     }
         
     }).catch (err=>{
-        alert('Assurez-vous que le serveur est bien en marche');
+        toast.error('Assurez-vous que le serveur est bien en marche',config.TOAST_OPTIONS);
     });
 }
